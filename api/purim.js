179607e306dba308ecd7a4df0eef18dbed163a7e
LAJewishEvents <lajevents.inbox@gmail.com>
2026-02-25 23:54:39 -0800

    Update purim.js

--- api/purim.js
+++ api/purim.js
@@ -1,1 +1,10 @@
-
+export default async function handler(req, res) {
+  try {
+    res.status(200).json({
+      status: "success",
+      message: "Purim API is live"
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
